1e1cb216387f932f1545962f02817ba59317869c
dlwjddus1112 <dlwjddus1112@naver.com>
2025-05-11 19:40:03 +0900

    [Feat] Swagger 초기 설정

---
+++ src/main/java/org/sopt/SOPT_36_COLLABORATION_SERVER_TEMU/global/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package org.sopt.SOPT_36_COLLABORATION_SERVER_TEMU.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "SOPT 36기 합동 세미나 데스크탑 웹 2팀 TEMU",
+                description = "Springdoc을 이용한 Swagger API 문서입니다.",
+                version = "1.0"
+        )
+)
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components());
+    }
+}
