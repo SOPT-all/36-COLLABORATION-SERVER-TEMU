f85e4d112ed8a0b8d3379324a3fabd84b1eb6682
dlwjddus1112 <dlwjddus1112@naver.com>
2025-05-11 15:51:34 +0900

    [Docs] application.yml 설정

---
+++ src/main/java/org/sopt/SOPT_36_COLLABORATION_SERVER_TEMU/Sopt36CollaborationServerTemuApplication.java
@@ -0,0 +1,13 @@
+package org.sopt.SOPT_36_COLLABORATION_SERVER_TEMU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sopt36CollaborationServerTemuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sopt36CollaborationServerTemuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sopt/SOPT_36_COLLABORATION_SERVER_TEMU/Sopt36CollaborationServerTemuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.SOPT_36_COLLABORATION_SERVER_TEMU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sopt36CollaborationServerTemuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
