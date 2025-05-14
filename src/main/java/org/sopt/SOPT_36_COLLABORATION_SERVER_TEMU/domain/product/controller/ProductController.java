ffe68e7c0a510d79e469f04090f2b5008d17c685
lezgititt <155799025+lezgititt@users.noreply.github.com>
2025-05-15 01:11:48 +0900

    [Fix] #10: Swagger 태그 변경

--- src/main/java/org/sopt/SOPT_36_COLLABORATION_SERVER_TEMU/domain/product/controller/ProductController.java
+++ src/main/java/org/sopt/SOPT_36_COLLABORATION_SERVER_TEMU/domain/product/controller/ProductController.java
@@ -43,7 +43,7 @@ public class ProductController {
     public BaseResponse<SearchResponse> getSearchedProduct(@RequestParam String keyword){
         return BaseResponse.ok(productService.getSearchedProduct(keyword));
     }
-    @Tag(name="특가상품 조회 API")
+    @Tag(name="메인페이지 조회 관련 API")
     @Operation(summary = "특가 상품 조회", description = "특가 상품 5개를 조회합니다.")
     @GetMapping("/promotion")
     public BaseResponse<PromotionResponse> getPromotion(){
