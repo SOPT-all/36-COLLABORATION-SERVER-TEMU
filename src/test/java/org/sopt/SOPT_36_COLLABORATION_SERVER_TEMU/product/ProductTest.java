777d3546d1a1c7a31c794b350d31c4b4bd9808b6
dlwjddus1112 <dlwjddus1112@naver.com>
2025-05-16 21:52:41 +0900

    [Refactor] #16 Product에 category 추가

---
+++ src/test/java/org/sopt/SOPT_36_COLLABORATION_SERVER_TEMU/product/ProductTest.java
@@ -0,0 +1,32 @@
+package org.sopt.SOPT_36_COLLABORATION_SERVER_TEMU.product;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.sopt.SOPT_36_COLLABORATION_SERVER_TEMU.domain.product.model.Category;
+import org.sopt.SOPT_36_COLLABORATION_SERVER_TEMU.domain.product.model.Product;
+import org.sopt.SOPT_36_COLLABORATION_SERVER_TEMU.domain.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+public class ProductTest {
+    @Autowired private ProductRepository productRepository;
+    @Test
+    public void createProductTest(){
+        //given
+        Product product = Product.builder()
+                .productName("가방")
+                .category(Category.GOODS)
+                .productDetails(null)
+                .productImages(null)
+                .productReviews(null)
+                .build();
+        productRepository.save(product);
+
+        //when
+        Product findProduct = productRepository.findById(3L).get();
+        //then
+        Assertions.assertThat(findProduct.getProductName()).isEqualTo("가방");
+    }
+}
